11659cad6cbc5ca63a974b108c3d238f5216ac81
kinoko Matumoto <superkinoko@icloud.com>
2020-03-30 10:57:43 +0900

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  endpointName: 're-more-auth.in-osaka.pigumer.gr.jp',
-  clientId: '6o7afsbgqajftikd08beq183q1',
-  redirectUri: 'http://localhost:4200'
-};
