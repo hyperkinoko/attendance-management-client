e75fcb06d30858cdf3e59e3659e3928b7f732402
kinoko Matumoto <superkinoko@icloud.com>
2020-03-30 10:51:50 +0900

    Edit .gitignore for firebase env

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  endpointName: 're-more-auth.in-osaka.pigumer.gr.jp',
+  clientId: '6o7afsbgqajftikd08beq183q1',
+  redirectUri: 'http://localhost:4200'
+};
